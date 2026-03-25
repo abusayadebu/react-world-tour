764d81d29f9b178dd8a9dd8998aebcafd049ab13
abusayadebu <abusayad743@gmail.com>
2026-03-25 12:19:39 +0600

    countries data loaded and displayed country name, population and flags

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { use } from 'react';
+import Country from '../country/Country';
 
-const Countries = () => {
+const Countries = ({countriesPromise}) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+    console.log(countries);
     return (
         <div>
-            <h4>countries here</h4>
+            <h2>countries here</h2>
+            <h4>Countries: {countries.length}</h4>
+            {
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
