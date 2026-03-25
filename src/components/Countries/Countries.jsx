d3e83f67ae632522e63c23ae29b54ca5bdc39010
abusayadebu <abusayad743@gmail.com>
2026-03-25 14:08:15 +0600

    visited countries state change and showes them in order list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,8 +1,16 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../country/Country';
 import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const handleVisitedCountries = (country) =>{
+        console.log('handle visiefjg clicked', country);
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
+
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     console.log(countries);
@@ -10,9 +18,18 @@ const Countries = ({countriesPromise}) => {
         <div>
             <h2>countries here</h2>
             <h4>Countries: {countries.length}</h4>
+            <h4>Total Visited Countries: {visitedCountries.length}</h4>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
-                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                countries.map(country => <Country key={country.cca3.cca3} 
+                    country={country}
+                    handleVisitedCountries={handleVisitedCountries}
+                    ></Country>)
             }
             </div>
         </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
     // console.log(country.area.area);
     const [visited, setVisited] = useState(false) 
 
@@ -12,6 +12,7 @@ const Country = ({country}) => {
         else{
             setVisited(true)
         }
+        handleVisitedCountries(country)
     }
     return (
         <div className={`country ${visited && 'visited-country'}`}>
